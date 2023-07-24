495932748252b3cb696304a62dadb8f6ebd39c07
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-07-24 19:14:05 +0900

    add components
    add some files(header, main, footer)

---
+++ Blochi/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import header from '../src/components/header/header.vue'
+import main from '../src/components/main/main.vue'
+import footer from '../src/components/footer/footer.vue'
+
+
+export default {
+  name: 'app',
+  components:{
+    'app-header': header,
+    'app-main': main,
+    'app-footer': footer
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Blochi/src/components/footer/footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>footer</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    
+    data () {
+      
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+
